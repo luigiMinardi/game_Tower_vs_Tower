206f0d4978d025ffff4388f4df4ac8c732534129
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-01 12:08:26 +0100

    Walk method is functional but still need to refactor to OO

--- js/spawns.js
+++ js/spawns.js
@@ -9,14 +9,20 @@ class Spawnable {
         this.y = "5em" // bottom to top
         this.width = "3em";
         this.height = "3em";
+
+        this.idCounter = 0
     }
 
     attack(opponent) {
         opponent.hp -= this.atk;
     }
 
-    walk(x) {
-        this.x += x;
+    walk(id, x) {
+        let elementToMove = document.getElementById(id)
+        console.log(this.x)
+        this.x = parseInt(this.x.replace("em","")) + x + "em";
+        elementToMove.style.left = this.x
+        console.log(elementToMove.style.left)
     }
 
     spawn() {
@@ -27,6 +33,8 @@ class Spawnable {
         minion.style.position = "absolute";
         minion.style.left = "5em"
         minion.style.bottom = "5em"
+        minion.id = this.name+this.idCounter
+        this.idCounter++
         let game = document.getElementById("game");
         game.appendChild(minion);
     }
@@ -34,4 +42,9 @@ class Spawnable {
 
 let test = new Spawnable("Junin", 100, 5, "white");
 
-test.spawn();
+async function spawnMob() {
+    await test.spawn();
+    await test.walk("Junin0", 4)
+}
+
+spawnMob()
