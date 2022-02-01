3635a10d0465778222e60e4357a2c03cf37fb783
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-01 13:37:02 +0100

    Attack method working, but need OO refactor

--- js/spawns.js
+++ js/spawns.js
@@ -14,13 +14,21 @@ class Spawnable {
     }
 
     attack(opponent) {
-        opponent.hp -= this.atk;
+        let opponentX = parseInt(opponent.x.replace("em", "")) - 3 + "em"
+        if (this.x == opponentX && this.y == opponent.y && opponent.hp > 0) {
+            opponent.hp -= this.atk;
+            console.log(opponent.hp)
+        } else if (opponent.hp > 0) {
+            console.log("your opponent died")
+        } else {
+            console.log("Without opponent in front of you")
+        }
     }
 
     walk(id, x) {
         let elementToMove = document.getElementById(id)
         console.log(this.x)
-        this.x = parseInt(this.x.replace("em","")) + x + "em";
+        this.x = parseInt(this.x.replace("em", "")) + x + "em";
         elementToMove.style.left = this.x
         console.log(elementToMove.style.left)
     }
@@ -33,7 +41,7 @@ class Spawnable {
         minion.style.position = "absolute";
         minion.style.left = "5em"
         minion.style.bottom = "5em"
-        minion.id = this.name+this.idCounter
+        minion.id = this.name + this.idCounter
         this.idCounter++
         let game = document.getElementById("game");
         game.appendChild(minion);
@@ -41,6 +49,8 @@ class Spawnable {
 }
 
 let test = new Spawnable("Junin", 100, 5, "white");
+let test2 = new Spawnable("Junao", 100, 15, "green")
+
 
 async function spawnMob() {
     await test.spawn();
