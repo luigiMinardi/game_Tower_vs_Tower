bd13f2e02eefb255e8bbed0e792499a5fcce1ef1
Luigi Minardi <lugminsinlag@gmail.com>
2022-02-04 18:52:07 +0100

    Refactoring the spawn to be in main and work with enemies and allies

--- js/main.js
+++ js/main.js
@@ -12,7 +12,52 @@ let changeScreen = (screenNumber) => {
     }
 }
 
+let numberOfMobsSpawned = 0;
+let objectOfMobsSpawned = {};
+let spawn = (name, hp, atk, sprite, type) => {
+    let id = name + numberOfMobsSpawned; //create mob (html div) id
+    let mob = '';
+    console.log(objectOfMobsSpawned);
+
+    let minion = document.createElement("div"); //creating the html element
+
+    if (type == "spawn") {
+        mob = new Spawnable(name, hp, atk, sprite, id); //instance of object spawnable
+        minion.style = `
+            width: ${mob.width};
+            height: ${mob.height};
+            background-color: ${mob.sprite};
+            position: absolute;
+            left: 5em;
+            bottom: 5em;
+        ` // setting style
+    } else if (type == "enemy") {
+        mob = new Enemy(name, hp, atk, sprite, id);
+        minion.style = `
+            width: ${mob.width};
+            height: ${mob.height};
+            background-color: ${mob.sprite};
+            position: absolute;
+            bottom: 5em;
+        ` // setting style
+    } else {
+        delete minion;
+        return console.log(`${type} is not a valid type of mob.`);
+    }
+    objectOfMobsSpawned[id] = mob; //adding mob to the object of mobs
+
+    minion.id = mob.id; //putting the id in the html
+
+    numberOfMobsSpawned++; //increasing the id number
+
+    let game = document.getElementById("game"); //selecting the "canvas"
+    game.appendChild(minion); //adding mob in game
+}
+
+spawn("Junin", 100, 5, "black", "enemy");
+objectOfMobsSpawned["Junin0"].walk(6);
+
 let playGame = (screenNumber) => {
-    
-    changeScreen(screenNumber)
+
+    changeScreen(screenNumber);
 }
--- js/spawns.js
+++ js/spawns.js
@@ -1,35 +1,8 @@
 class Spawnable extends Mob {
     constructor(name, hp, atk, sprite, id) {
-        super(name,hp,atk,sprite,id);
+        super(name, hp, atk, sprite, id);
     }
 }
 
-// new Spawnable("Junin", 100, 5, "white", "Junin1");
-
-let numberOfMobsSpawned = 0
-let objectOfMobsSpawned = {}
-let spawn = (name, hp, atk, color) => {
-    let id = name + numberOfMobsSpawned //create mob (html div) id
-    let spawned = new Spawnable(name, hp, atk, color, id); //instance of object spawnable
-    objectOfMobsSpawned[id] = spawned //adding mob to the object of mobs
-    console.log(objectOfMobsSpawned)
-    let minion = document.createElement("div") //creating the html element
-
-    minion.style = `
-        width: ${spawned.width};
-        height: ${spawned.height};
-        background-color: ${spawned.sprite};
-        position: absolute;
-        left: 5em;
-        bottom: 5em;
-    ` // setting style
-    minion.id = spawned.id //putting the id in the html
-
-    numberOfMobsSpawned++ //increasing the id number
-
-    let game = document.getElementById("game"); //selecting the "canvas"
-    game.appendChild(minion); //adding mob in game
-}
-
-spawn("Junin", 100, 5, "black", 10)
-objectOfMobsSpawned["Junin0"].walk(6)
+// new Spawnable("Junin", 100, 5, "white", 1);
+// new Spawnable(string, number, number, string, number);
